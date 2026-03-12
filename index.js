401cf6431a6665383c3a6153076887f0416f3b29
Myrto-Angeliki <72343229+Myrto-Angeliki@users.noreply.github.com>
2026-03-12 11:40:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
-function openNav(menu) {
+function toggleMenu(menu) {
   menu.classList.toggle('open');
-  document.getElementById("hamburger-icon").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  margin = document.getElementById("main").style.marginLeft;
+  if (margin == "250px"){
+    document.getElementById("hamburger-icon").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+  if (margin == "0"){
+    document.getElementById("hamburger-icon").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
 }
 
-function closeNav(menu) {
-  menu.classList.toggle('open');
-  document.getElementById("hamburger-icon").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
+
