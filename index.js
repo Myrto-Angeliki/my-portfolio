59a913b53dc870344ff2f1cd901bdbd6e28fed6a
Myrto-Angeliki <72343229+Myrto-Angeliki@users.noreply.github.com>
2026-03-12 11:05:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
-    document.getElementById("main").style.marginLeft = "250px";
+    margin = document.getElementById("main").style.marginLeft;
+    if (margin == "250px"){
+        document.getElementById("main").style.marginLeft = "0";
+    }
+    else{
+        document.getElementById("main").style.marginLeft = "250px";
+    }
 }
