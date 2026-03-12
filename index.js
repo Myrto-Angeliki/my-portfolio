97c185d24dceb71a232053ac4c5cb9f0e1992fab
Myrto-Angeliki <72343229+Myrto-Angeliki@users.noreply.github.com>
2026-03-12 11:54:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 
 function toggleMenu(menu) {
   menu.classList.toggle('open');
-  margin = document.getElementById("main").style.marginLeft;
-  if (margin == "250px"){
-    //document.getElementById("mobile-menu").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
-  if (margin == "0"){
-    //document.getElementById("mobile-menu").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
+  document.getElementById("main").toggle('open');
 }
 
 
